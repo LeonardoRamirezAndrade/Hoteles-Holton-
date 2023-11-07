9d19953b051c736fdcea0ee37bfb325234c7d159
Jonathan Vizcaino <jonayma0110@gmail.com>
2023-11-06 20:34:59 -0500

    Camibié prueba por medellin

--- versionF/pruebas.py
+++ versionF/pruebas.py
@@ -6,7 +6,7 @@ cadena = CadenaHolton("Holton")
 
 cadena.agregarHotel("tradicional", 6, 4,"Santa Marta")
 cadena.agregarHotel("tres estrellas", 6, 5,"Bógota")
-cadena.agregarHotel("boutique", 6, 3,"Calii")
+cadena.agregarHotel("boutique", 6, 3,"Medellín")
 
 cadena.mostrarHoteles()
 
