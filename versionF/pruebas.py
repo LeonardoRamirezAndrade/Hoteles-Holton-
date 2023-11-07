7b3e3a12a83a7f98b0e4dda682aca65aa746d503
Jonathan Vizcaino <jonayma0110@gmail.com>
2023-11-06 20:47:17 -0500

    Camibié agregué un nuevo hotel para prueba

--- versionF/pruebas.py
+++ versionF/pruebas.py
@@ -7,6 +7,7 @@ cadena = CadenaHolton("Holton")
 cadena.agregarHotel("tradicional", 6, 4,"Santa Marta")
 cadena.agregarHotel("tres estrellas", 6, 5,"Bógota")
 cadena.agregarHotel("boutique", 6, 3,"Medellín")
+cadena.agregarHotel("cinco estrellas", 6, 3,"Popayan")
 
 cadena.mostrarHoteles()
 
