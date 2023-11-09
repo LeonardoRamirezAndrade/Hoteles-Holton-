4b7d7fa91c823219ca2ff4fc115c4f703187f63c
Leonardo Ramirez <leonardoramireznegocios@gmail.com>
2023-11-09 01:50:01 -0500

    Se eliminaron las dos carpetas que no se usaran para el codigo final

--- PrimeraVersiónDelTrabajo/Empresa.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class Empresa:
-    def __init__(self, ubicacion, capacidad, precio):
-        self.ubicacion = ubicacion
-        self.capacidad = capacidad
-        self.precio = precio
-
